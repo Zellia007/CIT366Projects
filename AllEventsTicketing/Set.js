e2d6e1728aa9da0fbd00d11a682ed5a2b59ff566
KT <ric12052@byui.edu>
2017-01-17 08:48:51 -0700

    1/17/2017 second upload

--- AllEventsTicketing/Set.js
+++ AllEventsTicketing/Set.js
@@ -3,12 +3,24 @@ function Set() {
 	
 	this.intersection = function(listA, listB) {
     
-	   var resultList = new Array();
-       
-	   /*-------------------------------Insert your code here -------------------------------------*/
-	   
-	   /*-------------------------------Insert your code here -------------------------------------*/
-       
+	   var resultList = new Array(); // create a resultList array
+
+		if (listA === null || listB === null) { // check for invalid inputs
+			return null; // exit and return null to indicate an error
+		}
+
+		for (var i = 0: i < listA.length; i++) { // for every element in listA
+			var nextValue = listA[i]; // get next value in the list
+
+			//for every element in listB
+			for (var j = 0; j < listB.length; j++) {
+                if (listB[j] === nextValue) { // this listB element equals nextValue
+                    resultList.push(listB[j]); // add listB element to end of resultList
+                    break; // break out of (exit) the listB inner loop
+                }
+            } // end listB inner loop
+		} // end listA outer loop
+
 	   return resultList;
 	}
     
@@ -52,5 +64,7 @@ function Set() {
 	   return resultList;
 	}	
 	
+
+
 
 }
